6cad3b5cb7500a229d4a5e6933f678e1ab023e2b
liliana <slbustos6@misena.edu.co>
2015-06-11 11:39:01 -0500

    actualizar4r

--- formularioactualizar.php
+++ formularioactualizar.php
@@ -17,7 +17,13 @@ $valoracion= $_GET['valoracion'];
 $conexion = new PDO('sqlite:favoritos.db');
 
 $consulta=" SELECT * FROM favoritos WHERE usuario=".$usuario."'AND contrasena='".$contrasena."'AND categoria='".$categoria."'AND contenido='".$contenido."'AND valoracion'".$valoracion."";
+
+// select * from favoritos where usuario="liliana" and contrasena="liliana" and categoria= "personal" and contenido="liliana" and valoracion="liliana";
+
 $resultado = $conexion->query($consulta);
+
+var_dump($resultado);
+
 echo "
 <table border =1 width= 100%>
 <tr>
@@ -26,11 +32,42 @@ echo "
     <td>categoria</td>
     <td>comentario</td>
     <td>valoracion</td>
+    <td></td>
 </tr>
-</table>
-
 
 ";
 
+
+foreach ($resultado as $fila) {
+echo "
+
+<tr><form action='actualizarfavorito.php' method='post'>
+    <td><input type = 'text' name = 'titulo' value ='".$fila['titulo']."'></td>
+    <td><input type = 'text' name = 'direccion' value ='".$fila['direccion']."'></td>
+    <td><select name='categoria'>
+           <option value = 'salud'>salud</option>
+           <option value = 'trabajo'>trabajo</option>
+           <option value = 'hobby'>hobby</option>
+           <option value = 'personal'>personal</option>
+           <option value = 'otros '>otros</option>
+
+           <option value = '".$fila['categoria']."'Selected>".$fila['categoria']."</option>
+        </select>
+    </td>
+    <td><input type = 'text' name = 'contenido' value ='".$fila['contenido']."'></td>
+    <td><input type = 'text' name = 'valoracion' value ='".$fila['valoracion']."'></td> 
+    <td><input type='submit'></td>
+    </form>
+</tr>
+
+";
+
+}
+
+
+echo "</table>";
+var_dump($resultado);
+//cerramos la conexion
+$conexion = Null;
 
 ?>
